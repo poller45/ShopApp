b6197acf415b727b32382bb54f514418e2bc7e06
Pavlo Cherniakov <panya382@gmail.com>
2022-11-18 09:58:20 +0200

    making add/delete favorites

--- src/features/account/accountSlice.js
+++ src/features/account/accountSlice.js
@@ -1,35 +1,44 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const accountSlice = createSlice({
-  name: "account",
-  initialState: {
-    value: {
-      name: "",
-      email: "",
-      favourites: [],
-      status: false,
-    },
-  },
-  reducers: {
-    logout: (state) => {
-      state.value = {
-        name: "",
-        email: "",
-        favourites: [],
-        status: false,
-      };
-    },
-    addFavourite: (state, action) => {
-      state.value.favourites = [...state.value.favourites, action.payload];
-    },
-    login: (state, action) => {
-      state.value = {
-        name: action.payload.name,
-        favourites: action.payload.favourites,
-        status: true,
-      };
-    },
-  },
+	name: "account",
+	initialState: {
+		value: {
+			name: "",
+			email: "",
+			favourites: [],
+			status: false,
+		},
+	},
+	reducers: {
+		logout: (state) => {
+			state.value = {
+				name: "",
+				email: "",
+				favourites: [],
+				status: false,
+			};
+		},
+		addFavourite: (state, action) => {
+			if (
+				action.payload !==
+				state.value.favourites.find((fav) => fav === action.payload)
+			) {
+				state.value.favourites = [...state.value.favourites, action.payload];
+			} else {
+				state.value.favourites = state.value.favourites.filter(
+					(fav) => fav !== action.payload
+				);
+			}
+		},
+		login: (state, action) => {
+			state.value = {
+				name: action.payload.name,
+				favourites: action.payload.favourites,
+				status: true,
+			};
+		},
+	},
 });
 
 export const { login, logout, addFavourite } = accountSlice.actions;
